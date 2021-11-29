e8a8ed2864395eafb826f6367edee5459d28ceef
Rodrigo <rogomezpalacio@protonmail.com>
2021-11-29 16:20:19 -0600

    Add Service Extension Source files
    Motivation: files to be copied into the target directory

---
+++ support/serviceExtensionFiles/NotificationService.h
@@ -0,0 +1,11 @@
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Rodrigo Gomez-Palacio on 10/27/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
