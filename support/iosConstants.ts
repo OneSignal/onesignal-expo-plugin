5d88aed189987024ea694fa879119af33dbf1f37
Rodrigo <rogomezpalacio@protonmail.com>
2021-12-06 16:58:09 -0600

    Add Podfile Updater Helper

--- support/iosConstants.ts
+++ support/iosConstants.ts
@@ -1,2 +1,9 @@
 export const IPHONEOS_DEPLOYMENT_TARGET = "10.0";
 export const TARGETED_DEVICE_FAMILY = `\"1,2\"`;
+
+export const NSE_PODFILE_SNIPPET = `
+target 'OneSignalNotificationServiceExtension' do
+  pod 'OneSignalXCFramework', '>= 3.0', '< 4.0'
+end`;
+
+export const NSE_PODFILE_REGEX = /target 'OneSignalNotificationServiceExtension'/;
