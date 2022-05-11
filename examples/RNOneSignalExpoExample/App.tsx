307c84c554e81f9e21293e48be3036edf2c03a51
Rodrigo <rogomezpalacio@protonmail.com>
2022-05-11 18:33:30 -0500

    Update Example App

--- examples/RNOneSignalExpoExample/App.tsx
+++ examples/RNOneSignalExpoExample/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 
-import { initOneSignalClient } from './integrations/OneSignal';
-
-initOneSignalClient();
+const initOneSignal = () => {
+  OneSignal.setAppId("8fbf6c72-4b80-47a8-86ae-68de02259355");
+  OneSignal.setLogLevel(6, 0);
+  OneSignal.promptForPushNotificationsWithUserResponse(response => {
+    console.log(response);
+  });
+}
 
 export default function App() {
+  initOneSignal();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
