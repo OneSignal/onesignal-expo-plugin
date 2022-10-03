72907c24821b36622518dae8d9e4dab79acca898
Brian Smith <briansmith@onesignal.com>
2022-10-03 10:03:05 -0400

    Within `OneSignalNotificationServiceExtension` target, add `use_frameworks` line conditional on `ios.useFrameworks` property existing

--- support/iosConstants.ts
+++ support/iosConstants.ts
@@ -4,6 +4,7 @@ export const TARGETED_DEVICE_FAMILY = `"1,2"`;
 export const NSE_PODFILE_SNIPPET = `
 target 'OneSignalNotificationServiceExtension' do
   pod 'OneSignalXCFramework', '>= 3.0', '< 4.0'
+  use_frameworks! :linkage => podfile_properties['ios.useFrameworks'].to_sym if podfile_properties['ios.useFrameworks']
 end`;
 
 export const NSE_PODFILE_REGEX = /target 'OneSignalNotificationServiceExtension'/;
