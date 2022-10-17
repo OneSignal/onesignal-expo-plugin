8b4925cc298ac033e63b8d65ef61708adf8e6aeb
Brian Smith <briansmith@onesignal.com>
2022-10-17 13:44:57 -0400

    Provide ability to specify both large and small icons.

--- onesignal/withOneSignalAndroid.ts
+++ onesignal/withOneSignalAndroid.ts
@@ -3,15 +3,111 @@
  * @see https://documentation.onesignal.com/docs/react-native-sdk-setup#step-4-install-for-ios-using-cocoapods-for-ios-apps
  */
 
-import { ConfigPlugin } from '@expo/config-plugins';
+import { ConfigPlugin, withDangerousMod } from '@expo/config-plugins';
+import { generateImageAsync } from '@expo/image-utils';
 import { OneSignalLog } from '../support/OneSignalLog';
 import { OneSignalPluginProps } from '../types/types';
+import { resolve, parse } from 'path';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+
+const RESOURCE_ROOT_PATH = 'android/app/src/main/res/';
+
+// The name of each small icon folder resource, and the icon size for that folder.
+const SMALL_ICON_DIRS_TO_SIZE: { [name: string]: number } = { 
+        'drawable-mdpi': 24, 
+        'drawable-hdpi': 36,
+        'drawable-xhdpi': 48,
+        'drawable-xxhdpi': 72,
+        'drawable-xxxhdpi': 96
+  };
+
+// The name of each large icon folder resource, and the icon size for that folder.
+const LARGE_ICON_DIRS_TO_SIZE: { [name: string]: number } = { 
+        'drawable-xxxhdpi': 256
+  };
+
+const withSmallIcons: ConfigPlugin<OneSignalPluginProps> = (
+  config,
+  onesignalProps
+) => {
+
+  if(!onesignalProps.smallIcons && !config.notification?.icon)
+    return config
+
+  return withDangerousMod(config, [
+    'android',
+    async (config) => {
+      if(config.notification?.icon) {
+        await saveIconAsync(config.notification.icon, config.modRequest.projectRoot, SMALL_ICON_DIRS_TO_SIZE)
+      }
+
+      if(onesignalProps.smallIcons) {
+        await saveIconsArrayAsync(config.modRequest.projectRoot, onesignalProps.smallIcons, SMALL_ICON_DIRS_TO_SIZE);
+      }
+      return config;
+    },
+  ]);
+};
+
+const withLargeIcons: ConfigPlugin<OneSignalPluginProps> = (
+  config,
+  onesignalProps
+) => {
+
+  if(!onesignalProps.largeIcons)
+    return config
+
+  return withDangerousMod(config, [
+    'android',
+    async (config) => {
+      if(onesignalProps.largeIcons) {
+        await saveIconsArrayAsync(config.modRequest.projectRoot, onesignalProps.largeIcons, LARGE_ICON_DIRS_TO_SIZE);
+      }
+      return config;
+    },
+  ]);
+};
+
+async function saveIconsArrayAsync(projectRoot: string, icons: string[], dirsToSize: { [name: string]: number }) {
+  for(const icon of icons) {
+    await saveIconAsync(icon, projectRoot, dirsToSize);
+  }
+}
+
+async function saveIconAsync(icon: string, projectRoot: string, dirsToSize: { [name: string]: number }) {
+  const name = parse(icon).name;
+
+  OneSignalLog.log("Saving icon " + icon + " as drawable resource " + name);
+
+  for(const iconResourceDir in dirsToSize) {
+    const path = resolve(projectRoot, RESOURCE_ROOT_PATH, iconResourceDir);
+
+    if (!existsSync(path)) {
+      mkdirSync(path, { recursive: true });
+    }
+
+    const resizedIcon = (
+      await generateImageAsync(
+        { projectRoot, cacheType: 'onesignal-icon' },
+        {
+          src: icon,
+          width: dirsToSize[iconResourceDir],
+          height: dirsToSize[iconResourceDir],
+          resizeMode: 'cover',
+          backgroundColor: 'transparent',
+        }
+      )
+    ).source;
+
+    writeFileSync(resolve(path, name + '.png'), resizedIcon);
+  }
+}
 
 export const withOneSignalAndroid: ConfigPlugin<OneSignalPluginProps> = (
   config,
   props,
 ) => {
-  //commented out until https://github.com/expo/eas-cli/issues/1226 is confirmed fixed
-  //OneSignalLog.log("No specific actions required for OneSignal on Android...");
+  config = withSmallIcons(config, props);
+  config = withLargeIcons(config, props);
   return config;
 };
--- types/types.ts
+++ types/types.ts
@@ -3,11 +3,31 @@
  */
  export type OneSignalPluginProps = {
   /**
-   * (iOS only) Environment name and bundle identifier
+   * (required) Used to configure APNs environment entitlement. "development" or "production"
    */
   mode: Mode;
+
+  /**
+   * (optional) Used to configure Apple Team ID. You can find your Apple Team ID by running expo credentials:manager e.g: "91SW8A37CR" 
+   */
   devTeam: string;
+
+  /**
+   * (optional) Target IPHONEOS_DEPLOYMENT_TARGET value to be used when adding the iOS NSE. A deployment target is nothing more than
+   * the minimum version of the operating system the application can run on. This value should match the value in your Podfile e.g: "12.0".
+   */
   iPhoneDeploymentTarget: string;
+
+  /**
+   * (optional) The small notification icons for Android.  Images will be automatically scaled up/down, the recommended size
+   * is 96x96 to always scale down.
+   */
+  smallIcons?:            string[];
+
+  /**
+   * (optional) The large notification icons for Android. Images will be automatically scaled up/down to 256x256.
+   */
+  largeIcons?:            string[];
 };
 
 /**
