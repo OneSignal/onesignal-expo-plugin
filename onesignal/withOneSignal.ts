c1051c3091638d7d20cb12a3e0f3d5ea1002f1ed
Rodrigo Gomez Palacio <rodrigo@onesignal.com>
2023-09-25 10:57:41 -0700

    Check that plugin props are being provided
    Motivation: the property "mode" is required. Currently, because we don't check for this, nodejs is throwing a runtime error when trying to access smallIcons (optional prop) when the props object is undefined.
    
    This is confusing for folks who first set up and try running without setting a props object to the plugin array.
    
    Clear error message will be much more helpful.

--- onesignal/withOneSignal.ts
+++ onesignal/withOneSignal.ts
@@ -9,6 +9,13 @@ import { withOneSignalAndroid } from './withOneSignalAndroid';
 import { withOneSignalIos } from './withOneSignalIos';
 
 const withOneSignal: ConfigPlugin<OneSignalPluginProps> = (config, props) => {
+  // if props are undefined, throw error
+  if (!props) {
+    throw new Error(
+      'You are trying to use the OneSignal plugin without any props. Property "mode" is required. Please see https://github.com/OneSignal/onesignal-expo-plugin for more info.'
+    );
+  }
+
   config = withOneSignalIos(config, props);
   config = withOneSignalAndroid(config, props);
 
