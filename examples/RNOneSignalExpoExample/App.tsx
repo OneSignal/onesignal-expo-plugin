5a817764e26b4ba14d813aba54d6b5219981de49
Matiluko Opeyemi Emmanuel <opmat01@yahoo.com>
2024-02-01 19:17:32 +0100

    Update App.tsx
    Update the code to reflect changes in plugin version 2.0.2

--- examples/RNOneSignalExpoExample/App.tsx
+++ examples/RNOneSignalExpoExample/App.tsx
@@ -1,13 +1,21 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import OneSignal from 'react-native-onesignal';
+import { LogLevel, OneSignal } from "react-native-onesignal";
 
 const initOneSignal = () => {
-  OneSignal.setAppId("8fbf6c72-4b80-47a8-86ae-68de02259355");
-  OneSignal.setLogLevel(6, 0);
-  OneSignal.promptForPushNotificationsWithUserResponse(response => {
-    console.log(response);
-  });
+  OneSignal.initialize("8fbf6c72-4b80-47a8-86ae-68de02259355");
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+  
+  // Also need to enable notifications to complete OneSignal setup
+  if (await OneSignal.Notifications.canRequestPermission()) {
+    let permission = OneSignal.Notifications.hasPermission();
+    if (!permission) {
+      await OneSignal.Notifications.requestPermission(true);
+    }
+  }
+  OneSignal.setConsentRequired(true);
+  OneSignal.setConsentGiven(true);
 }
 
 export default function App() {
