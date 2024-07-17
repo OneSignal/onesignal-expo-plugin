14327e29665118a07aa451216520f649210daa45
Elliot Mawby <emawby@gmail.com>
2024-07-17 13:59:25 -0700

    Fix the example app

--- examples/RNOneSignalExpoExample/App.tsx
+++ examples/RNOneSignalExpoExample/App.tsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { LogLevel, OneSignal } from "react-native-onesignal";
+import Constants from 'expo-constants';
 
 const initOneSignal = () => {
-  OneSignal.initialize("8fbf6c72-4b80-47a8-86ae-68de02259355");
+  var appId = Constants.expoConfig?.extra?.oneSignalAppId
+  console.log(appId)
+  OneSignal.initialize(appId || "8fbf6c72-4b80-47a8-86ae-68de02259355");
   OneSignal.Debug.setLogLevel(LogLevel.Verbose);
-  
+  OneSignal.setConsentRequired(true);
+  OneSignal.setConsentGiven(true);
+  requestPermission();
+}
+
+async function requestPermission() {
   // Also need to enable notifications to complete OneSignal setup
   if (await OneSignal.Notifications.canRequestPermission()) {
-    let permission = OneSignal.Notifications.hasPermission();
+    let permission = OneSignal.Notifications.getPermissionAsync();
     if (!permission) {
       await OneSignal.Notifications.requestPermission(true);
     }
   }
-  OneSignal.setConsentRequired(true);
-  OneSignal.setConsentGiven(true);
 }
 
 export default function App() {
