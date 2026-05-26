7b41880786d596981e32b3ea2d8fd5e8b3c1b40b
ChromeQ <originaldavz@gmail.com>
2026-05-26 13:41:01 +1000

    Export as named export Update readme

--- src/onesignal/plugin.ts
+++ src/onesignal/plugin.ts
@@ -1,7 +1,6 @@
 import { name } from "../../package.json";
 import { type OneSignalPluginProps as PluginProps } from "../types/types";
 
-export default (props: PluginProps): [typeof name, PluginProps] => [
-  name,
-  props,
-];
+export const oneSignalExpoPlugin = (
+  props: PluginProps,
+): [typeof name, PluginProps] => [name, props];
